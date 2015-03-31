e7e3a4895e274348732c8c9c36dfd9a8377df882
pritz2 <pritzluke@gmail.com>
2015-03-31 14:38:05 -0500

    Check out now possible

--- leave_location.php
+++ leave_location.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
 unset($_SESSION["locationID"]);
+
+include './include/database_info.php';
+$conn = mysql_connect("$host:$port",$user,$password) or die("Connection error");
+$db_selected = mysql_select_db($db,$conn) or die(mysql_error($db));
+
+$sql = "DELETE FROM CheckIn WHERE studentID = '".$_SESSION["id"]."';";
+
+mysql_query( $sql, $conn );
+
 header("Location:dashboard.php");
 ?>
