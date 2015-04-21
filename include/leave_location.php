6e00dfebef09a806a55c56ed2f67bf23d8175d8d
pritz2 <pritzluke@gmail.com>
2015-04-21 14:08:45 -0500

    Logging out now checks you out of your location

--- include/leave_location.php
+++ include/leave_location.php
@@ -8,5 +8,8 @@ $sql = "DELETE FROM CheckIn WHERE studentID = '".$_SESSION["id"]."';";
 
 mysql_query( $sql, $conn );
 
-header("Location:../dashboard.php");
+if(isset($_SESSION['logout']))
+	header("Location:logout.php");
+else
+	header("Location:../dashboard.php");
 ?>
--- include/logout.php
+++ include/logout.php
@@ -1,5 +1,12 @@
 <?php
+
 session_start();
+$_SESSION['logout'] = true;
+if(isset($_SESSION['locationID'])){
+	header("Location:leave_location.php");
+	exit();
+}
+
 $_SESSION = array();
 session_destroy();
 
