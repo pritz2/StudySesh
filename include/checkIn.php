86a4b790210177e108464e2c09c5363034c91008
dtewers <dtewers94@gmail.com>
2015-04-22 19:58:11 -0500

    Added timeVisited to checkin

--- include/checkIn.php
+++ include/checkIn.php
@@ -6,10 +6,11 @@ if(count($_POST)>0) {
 	include_once 'db_connect.php';
 
 	$sql = 'INSERT INTO CheckIn '.
-			'(studentID,locationID,classID) '.
-			"VALUES ( '".$_SESSION["id"]."', '".$_POST["locationID"]."', '".$_POST["classID"]."');";
+			'(studentID,locationID,classID, timeVisited) '.
+			"VALUES ( '".$_SESSION["id"]."', '".$_POST["locationID"]."', '".$_POST["classID"]."', CURRENT_TIMESTAMP());";
 
 	mysql_query( $sql, $conn );
+
 
 	$_SESSION["locationID"] = $_POST["locationID"];
     $_SESSION["classID"] = $_POST["classID"];
