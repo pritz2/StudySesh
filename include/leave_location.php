410727adc9cbd80b15c0043fb315afad28d9bf29
dtewers <dtewers94@gmail.com>
2015-04-22 21:48:16 -0500

    Log check in time when location is left

--- include/leave_location.php
+++ include/leave_location.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
-unset($_SESSION["locationID"]);
 
 include_once 'db_connect.php';
 
-$sql = "DELETE FROM CheckIn WHERE studentID = '".$_SESSION["id"]."';";
+$sql = 'INSERT INTO LoggedCheckIn '.
+    '(studentID, classID, locationID, totalTime) '.
+    "VALUES ( '".$_SESSION["id"]."', '".$_SESSION["classID"]."', '".$_SESSION["locationID"].
+    "', TIMESTAMPDIFF( MINUTE, (SELECT timeVisited FROM CheckIn WHERE studentID = '".$_SESSION["id"]."'), CURRENT_TIMESTAMP() ));";
 
 mysql_query( $sql, $conn );
+
+$sq2 = "DELETE FROM CheckIn WHERE studentID = '".$_SESSION["id"]."';";
+
+mysql_query( $sq2, $conn );
+
+unset($_SESSION["locationID"]);
 
 if(isset($_SESSION['logout']))
 	header("Location:logout.php");
